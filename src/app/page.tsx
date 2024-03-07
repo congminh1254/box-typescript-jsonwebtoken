92587fac57789f8408744da085bc5faf0c47def4
Minh Nguyen Cong <mcong@box.com>
2024-03-07 06:07:32 +0100

    Update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BoxSDK from 'box-node-sdk'
+import { BoxClient } from 'box-typescript-sdk-gen'
 import { BoxJwtAuth, JwtConfig } from 'box-typescript-sdk-gen/lib/jwtAuth.generated.js'
 import jsonwebtoken from 'jsonwebtoken'
 // const { BoxJwtAuth, JwtConfig } = await import('box-typescript-sdk-gen/lib/jwtAuth.generated.js')
@@ -11,9 +12,9 @@ export default function HomePage() {
     'use server'
 
     const boxSDK = BoxSDK.getPreconfiguredInstance(
-      JSON.parse(process.env.BOX_CONFIG),
+      JSON.parse(atob(process.env.JWT_CONFIG_BASE_64 || '')),
     )
-    const boxClient = boxSDK.getAppAuthClient('user', process.env.BOX_APP_USER_ID)
+    const boxClient = boxSDK.getAppAuthClient('user', process.env.ADMIN_USER_ID)
 
     const token = await boxClient.exchangeToken(
       ['item_preview'],
@@ -29,23 +30,28 @@ export default function HomePage() {
     'use server'
 
     console.log({ jsonwebtoken })
-
-    const boxJwtConfig = JwtConfig.fromConfigJsonString(process.env.BOX_CONFIG)
+    console.log(atob(process.env.JWT_CONFIG_BASE_64 || ''))
+    const boxJwtConfig = JwtConfig.fromConfigJsonString(atob(process.env.JWT_CONFIG_BASE_64 || ''))
     const boxJwtAuth = await new BoxJwtAuth({ config: boxJwtConfig })
-      .asUser(process.env.BOX_APP_USER_ID)
+      .asUser(process.env.ADMIN_USER_ID || '')
 
     // Retrieve a token to avoid error: "No access token is available. Make an
     // API call to retrieve a token before calling this method."
     await boxJwtAuth.retrieveToken()
 
-    const token = await boxJwtAuth.downscopeToken(
-      ['item_preview'],
-      `https://api.box.com/2.0/files/${process.env.BOX_FILE_ID}`,
-    )
+    const client = new BoxClient({
+      auth: boxJwtAuth,
+    })
+    console.log(await client.users.getUserMe())
 
-    console.log({ token })
+    // const token = await boxJwtAuth.downscopeToken(
+    //   ['item_preview'],
+    //   `https://api.box.com/2.0/files/${process.env.BOX_FILE_ID}`,
+    // )
 
-    return token.accessToken
+    // console.log({ token })
+
+    // return token.accessToken
   }
 
   return (
